0e3b6542c49491d269f230cc0b737681b05ea104
victorolv <victorolvdev@gmail.com>
2023-05-09 20:00:01 -0300

    password / stats

--- functions.php
+++ functions.php
@@ -1,12 +1,34 @@
 <?php 
 
-remove_action('rest_api_init','create_initial_rest_routes',99);
+// remove_action('rest_api_init','create_initial_rest_routes',99);
+
+add_filter('rest_endpoints', function($endpoints){
+
+  unset($endpoints['/wp/v2/users']);
+  unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+  return $endpoints;
+});
 
 $dirbase = get_template_directory();
 
 
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_delete.php';
+require_once $dirbase . '/endpoints/photo_get.php';
+
+require_once $dirbase . '/endpoints/password.php';
+
+require_once $dirbase . '/endpoints/stats_get.php';
+
+
+
+update_option('large_size_w', 1000);
+update_option('large_size_h', 1000);
+update_option('large_crop', 1);
+
 
 function change_api(){
   return 'json';
